686b61be4204f26696a6e95fbce12dc04f5daa58
nabilalaa <nabilalaa06@gmail.com>
2020-10-22 14:58:02 +0200

    tasks

---
+++ Python Operators Tasks.py
@@ -0,0 +1,17 @@
+# Python Operators Tasks
+
+# What is the differences between = , == ?
+# ( = ) is a assignment operator
+# ( == ) is a comparison operator
+# -----------------
+
+# What do we mean by using != ?
+# ( != ) mean no equal
+# -----------------
+
+# What is the operator precedence ?
+# ()
+# **
+# * , /
+# + , -
+# -----------------
---
+++ Python Variables Tasks.py
@@ -0,0 +1,21 @@
+# Python Variables Tasks
+
+# Define a variable of type bool ?
+question = True
+# -----------------
+
+# What Is the benefit of the type method ?
+# to we know type of variable
+# -----------------
+
+# Can we use semi colon ; with python ?
+# yes we can
+# -----------------
+
+# Python is interpreted or compiled ?
+# python is interpreted
+# -----------------
+
+# What is the differences between low level & high level ?
+# low level is computer language , hard to program , easy to be executed
+# high level is easy to program , easy to be corrected , takes less time to be coded
