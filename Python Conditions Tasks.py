3f8f4479e1dafe2cf1befcc62a4ee16fdcc76e45
nabilalaa <nabilalaa06@gmail.com>
2020-10-26 08:27:13 +0200

    new task

---
+++ Python Conditions Tasks.py
@@ -0,0 +1,34 @@
+# Python Conditions Tasks
+
+# In which cases we will use all ?
+# When we have multiple conditions
+# -----------------
+
+# What is the differences between all , and ?
+# There is no difference between them but (all function) help save time
+# -----------------
+
+# What is the differences between any , or ?
+# There is no difference between them but (any function) help save time
+# -----------------
+
+# If we need all the conditions to be true we will use ?
+# we will use and / all()
+
+# What is the differences between if , elif ?
+# (if) can be used to start condition
+# (elif) using to write many condition
+# -----------------
+
+# What is the differences between elif else  ?
+# (elif) using to write many condition
+# (else) can be used when all condition failed
+# -----------------
+
+# Can we use more than one elif ?
+# yes we can
+# -----------------
+
+# Can we use more than one else ?
+# we cannot
+
